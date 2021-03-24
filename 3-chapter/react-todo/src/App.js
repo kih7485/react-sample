1d59528b9086958b79badd0641dfc91ed60ecd90
inhan <inhan@inhanui-MacBookPro.local>
2021-03-24 16:46:31 +0900

    chapter-3

---
+++ 3-chapter/react-todo/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+let color = 'red';
+export default function App(){
+  const [color, setColor] = useState('red');
+  function onClick(){
+    setColor('blue')
+  }
+  return (
+    <button style={{backgroundColor:color}} onClick={onClick}>
+      좋아요
+    </button>
+  )
+}
