c904329ee562e89ad0761c6607fa2205fc7166df
inhan <korea4127@gmail.com>
2022-01-25 01:40:03 +0900

    검색 탭 수정

--- vanila/js/views/TabView.js
+++ vanila/js/views/TabView.js
@@ -1,4 +1,15 @@
+import { qs } from "../helpers.js";
 import View from "./View.js";
+
+const TabType = {
+  KEYWORD: "KEYWORD",
+  HISTORY: "HISTORY",
+};
+
+const TabLabel = {
+  [TabType.KEYWORD]: "추천 검색어",
+  [TabType.HISTORY]: "최근 검색어",
+};
 
 export default class TabView extends View {
   constructor() {
@@ -9,7 +20,28 @@ export default class TabView extends View {
     this.element.innerHTML = this.template.getTabList();
     super.show();
   }
-  getTabList() {}
 }
 
-class Template {}
+class Template {
+  getTabList() {
+    return `
+        <ul class="tabs">
+        ${Object.values(TabType)
+          .map((tabType) => ({
+            tabType,
+            tabLabel: TabLabel[tabType],
+          }))
+          .map(this._getTab)
+          .join("")}
+        
+        </ul>
+        `;
+  }
+  _getTab({ tabType, tabLabel }) {
+    return `
+        <li data-tab="${tabType}">
+            ${tabLabel}
+        </li>
+        `;
+  }
+}
