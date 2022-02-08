dc53a9d5fbe1b6f9bce543fc7aaebe4600e13066
inhan <korea4127@gmail.com>
2022-02-08 23:27:28 +0900

    헤더 컴포넌트 분리

--- 5-component/src/App.jsx
+++ 5-component/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <> Todo: App Component</>
+    <> <Header/></>
   )
 }
 
